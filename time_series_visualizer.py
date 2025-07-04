c1588e9585e450d2fa88842b7e4827641f4a7a22
Matthew Chan <chan.matthewdavid@gmail.com>
2025-07-04 19:43:58 +0000

    Finished box and line

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,14 +5,28 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv(
+    'fcc-forum-pageviews.csv', 
+    parse_dates=['date'], # date column
+    index_col ='date'
+    )
 
 # Clean data
-df = None
+# Filterng out days when page views were in the top 2.5% of dataset, or bottom 2.5% of dataset
+# Layman's Terms: Dropping the top 2.5% and bottom 2.5% of views
+df = df[
+    (df['value'] >= df['value'].quantile(0.025)) &
+    (df['value'] <= df['value'].quantile(0.975))
+]
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.plot(df.index, df['value'], color='red')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
 
 
@@ -23,14 +37,30 @@ def draw_line_plot():
     return fig
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = None
+    # Copy and modify data for monthly bar plot. Find each months average page views
+    df_bar = df.copy()
+    
+    # We need multiple columns to make grouped bars, so...
+    # Add a column for years
+    # Add a column for months
+
+    df_bar['months'] = df_bar.index.month
+    df_bar['years'] = df_bar.index.year
 
     # Draw bar plot
 
+    pivot_table = df_bar.pivot_table(index = 'years', columns = 'months')
+    fig, ax = plt.subplots(figsize=(12, 6))
+    pivot_table.plot(kind='bar', ax=ax)
 
-
-
+    ax.set_xlabel('Years')
+    ax.set_ylabel('Average Page Views')
+    ax.set_title('Monthly Average Page Views by Year')
+        
+    # Legend 
+    month_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    ax.legend(labels=month_labels, title='Months')
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
